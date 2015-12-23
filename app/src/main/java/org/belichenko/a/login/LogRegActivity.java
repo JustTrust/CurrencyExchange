37efc19370d4193951681189a86f7438ffa24170
JustTrust <a.belichenko@gmail.com>
2015-12-23 21:48:44 +0200

    rebuild for fragments

---
+++ app/src/main/java/org/belichenko/a/login/LogRegActivity.java
@@ -0,0 +1,15 @@
+package org.belichenko.a.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.belichenko.a.currencyexchange.R;
+
+public class LogRegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_reg);
+    }
+}
