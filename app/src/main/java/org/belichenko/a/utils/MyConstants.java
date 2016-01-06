81fa2f41d7432ecd35a5e90ef9f874c160518518
JustTrust <a.belichenko@gmail.com>
2016-01-06 20:06:20 +0200

    final version on fragments, next on Butter Knife

--- app/src/main/java/org/belichenko/a/utils/MyConstants.java
+++ app/src/main/java/org/belichenko/a/utils/MyConstants.java
@@ -9,7 +9,6 @@ public interface MyConstants {
     String USER_IS_LOGIN = "org.belichenko.currencyExchange.userIsLogin";
 
     String MAIN_PREFERENCE = "org.belichenko.currencyExchange.mainPreference";
-    String ORIENTATION = "org.belichenko.currencyExchange.orientation";
 
     String EDIT_LOGIN_NAME = "org.belichenko.currencyExchange.editLoginName";
     String EDIT_LOGIN_PASS = "org.belichenko.currencyExchange.editLoginPass";
